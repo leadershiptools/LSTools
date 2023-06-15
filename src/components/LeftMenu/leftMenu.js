1be3fd81abec11e24f50e59f53683aeea88db3e7
rai soares <rai.soares.dev@hotmail.com>
2023-06-15 13:56:37 -0300

    feat: responsive team screen

--- src/components/LeftMenu/leftMenu.js
+++ src/components/LeftMenu/leftMenu.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../LeftMenu/leftMenu.styles.css";
 import "../Styles/commons.styles.css";
 import Typography from "@mui/material/Typography";
@@ -6,52 +6,76 @@ import Team from "@mui/icons-material/WorkspacesOutlined";
 import People from "@mui/icons-material/PeopleAltOutlined";
 import { Button } from "@mui/material";
 import { useLocation, useNavigate } from "react-router-dom";
-
+import MenuIcon from "@mui/icons-material/Menu";
 const LeftMenu = ({ user }) => {
   const location = useLocation();
   const navigate = useNavigate();
+  const [isMobile, setIsMobile] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (window.outerWidth <= 1024) setIsMobile(true);
+  }, []);
+
+  console.log(isMobile);
   return (
-    <aside>
-      <div role="menu" className="container">
-        <header>
-          <Typography className="headerName" fontWeight={700} fontSize="20px">
-            {user?.name}
-          </Typography>
-          <Typography className="headerEmail" fontSize="14px" fontWeight={400}>
-            {user?.email}
-          </Typography>
-        </header>
-        <main>
-          <ol>
-            <li className="menuitem">
-              <Button
-                className={`navigationBtn ${
-                  location.pathname.includes("team") && "active"
-                }`}
-                startIcon={<Team />}
-                onClick={() => navigate("/LSTools/team")}
-              >
-                <Typography>Team</Typography>
-              </Button>
-            </li>
-            <li className="menuitem">
-              <Button
-                className={`navigationBtn ${
-                  location.pathname.includes("people") && "active"
-                }`}
-                startIcon={<People />}
-                disabled={true}
-              >
-                <Typography>People</Typography>
-              </Button>
-            </li>
-          </ol>
-        </main>
-      </div>
-      <div role="separator">
-        <hr className="line" />
-      </div>
-    </aside>
+    <>
+      {isMobile && (
+        <MenuIcon className="menu-icon" onClick={() => setIsOpen(true)} />
+      )}
+      <aside className={`${isOpen && "isOpen"}`}>
+        <div role="menu" className="container">
+          <header>
+            <Typography className="headerName" fontWeight={700} fontSize="20px">
+              {user?.name}
+            </Typography>
+            <Typography
+              className="headerEmail"
+              fontSize="14px"
+              fontWeight={400}
+            >
+              {user?.email}
+            </Typography>
+          </header>
+          <main>
+            <ol>
+              <li className="menuitem">
+                <Button
+                  className={`navigationBtn ${
+                    location.pathname.includes("team") && "active"
+                  }`}
+                  startIcon={<Team />}
+                  onClick={() => navigate("/LSTools/team")}
+                >
+                  <Typography>Team</Typography>
+                </Button>
+              </li>
+              <li className="menuitem">
+                <Button
+                  className={`navigationBtn ${
+                    location.pathname.includes("people") && "active"
+                  }`}
+                  startIcon={<People />}
+                  disabled={true}
+                >
+                  <Typography>People</Typography>
+                </Button>
+              </li>
+            </ol>
+          </main>
+        </div>
+        {!isMobile ? (
+          <div role="separator">
+            <hr className="line" />
+          </div>
+        ) : (
+          <div
+            onClick={() => setIsOpen(false)}
+            className={`menu-overlay ${isOpen && "isOpen"}`}
+          />
+        )}
+      </aside>
+    </>
   );
 };
 export default LeftMenu;
